06a88f7cbf9e4689c5f350c9af05ae49b6792f47
jdenzer00 <166157340+jdenzer00@users.noreply.github.com>
2024-04-10 17:29:11 +0200

    Removes shell scripts

--- TESTS-SPASS/modules/createmodules.sh
+++
@@ -1,3 +0,0 @@
-../../../sofronie/DL/cel/cel < input-galen-mod.test > output-galen-mod.txt
-../../../sofronie/DL/cel/cel < input-go-mod.test > output-go-mod.txt
-../../../sofronie/DL/cel/cel < input-nci-mod.test > output-nci-mod.txt
